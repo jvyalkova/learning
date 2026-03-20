c7b69b5d7a154a7047022b317a8566b2bfcfc41f
Vyalkova J.A. <j.vyalkova@mail.ru>
2026-03-20 16:07:25 +0300

    Добавлена авторизация, личный кабинет, покупка акций, кэширование

--- learning/settings.py
+++ learning/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,12 +55,13 @@ ROOT_URLCONF = 'learning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -116,9 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / 'mediafiles/'
+MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = 'stock:account'
+LOGIN_URL = 'login'
--- stock/admin.py
+++ stock/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
-from stock.models import Stock, Currency
+from stock.models import (Stock, Currency, Account, 
+    AccountCurrency, AccountStock)
+
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
-    list_display = ("ticker", "name", "description")
+    list_display = ("ticker", "name", "description", "logo")
+
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = ("name", "ticker", "sign")
+
+
+# Inline классы для отображения в Account
+class AccountCurrencyInline(admin.TabularInline):
+    model = AccountCurrency
+
+
+class AccountStockInline(admin.TabularInline):
+    model = AccountStock
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [AccountCurrencyInline, AccountStockInline]
+
+
+@admin.register(AccountCurrency)
+class AccountCurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountStock)
+class AccountStockAdmin(admin.ModelAdmin):
+    pass
